b29e18aa15fa06929f15b9beb6b203aba0f2e0b8
AndrewGorlo <ande-sparrow@ya.ru>
2025-09-14 01:20:08 +0300

    successfully nginx and docker compose config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,12 +5,10 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideKeycloakAngular } from './auth/keycloak-provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideKeycloakAngular(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
   ],
--- frontend/src/app/components/chat-board/chat-board.component.ts
+++ frontend/src/app/components/chat-board/chat-board.component.ts
@@ -1,6 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { WebSocketService } from '../../websocket/websocket.service';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { TuiButton } from '@taiga-ui/core';
@@ -11,19 +9,8 @@ import { TuiButton } from '@taiga-ui/core';
   styleUrls: ['./chat-board.component.css'],
   imports: [FormsModule, TuiButton],
 })
-export class ChatBoardComponent implements OnInit {
-  private webSocketService = inject(WebSocketService);
-  message = toSignal(this.webSocketService.messageSubject);
-
+export class ChatBoardComponent {
   text = '';
 
   constructor() {}
-
-  ngOnInit() {
-    this.webSocketService.connect();
-  }
-
-  sendMessage(message: string) {
-    this.webSocketService.sendMessage(message);
-  }
 }
